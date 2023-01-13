1d3761356a25254a09866bbd74cced4ba4a03fc7
Balázs Orbán <info@balazsorban.com>
2023-01-13 17:08:08 +0100

    fixes

--- docs-nextra/next.config.mjs
+++ docs-nextra/next.config.mjs
@@ -1,6 +1,25 @@
 import withNextra from "nextra"
+import RemarkLinkRewrite from "remark-link-rewrite"
 
 export default withNextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
+  mdxOptions: {
+    remarkPlugins: [
+      [
+        RemarkLinkRewrite,
+        {
+          replacer(url) {
+            if (url.endsWith(".md")) {
+              url = url.replace(".md", "")
+            }
+            if (url.startsWith("https://authjs.dev")) {
+              url = url.replace("https://authjs.dev", "")
+            }
+            return url
+          },
+        },
+      ],
+    ],
+  },
 })()
--- docs-nextra/theme.config.jsx
+++ docs-nextra/theme.config.jsx
@@ -1,9 +1,43 @@
+/** @type {import("nextra-theme-docs").DocsThemeConfig} */
 export default {
-  logo: <span>Auth.js Docs</span>,
-  toc: {
-    extraContent: <></>,
+  logo: (
+    <>
+      <img
+        height={28}
+        width={28}
+        src="https://authjs.dev/img/logo/logo-sm.png"
+      />
+      <span style={{ marginLeft: 8 }}>Auth.js</span>
+    </>
+  ),
+  toc: { extraContent: <></> },
+  darkMode: true,
+  primaryHue: {
+    light: 260,
+    dark: 20,
   },
-  project: {
-    link: "https://authjs.dev",
+  banner: {
+    text: (
+      <>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://next-auth.js.org"
+        >
+          NextAuth.js
+        </a>{" "}
+        is becoming Auth.js! 🎉 We're creating Authentication for the Web.
+        Everyone included. Starting with SvelteKit, check out{" "}
+        <a href="/reference/sveltekit">the docs</a>
+      </>
+    ),
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Auth.js",
+      description: "Authentication for the Web.",
+    }
+  },
+  docsRepositoryBase: "https://github.com/nextauthjs/next-auth/docs",
+  project: { link: "https://github.com/nextauthjs/next-auth" },
 }
